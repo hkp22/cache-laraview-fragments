b9448ad471974f6c200aefeddc7e89fb6a99b1c9
Harish Kumar <harish.pra22@gmail.com>
2018-09-18 18:02:01 +0530

    Package service provider

---
+++ src/CacheLaraViewFragmentServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Hkp22\CacheLaraViewFragments;
+
+use Illuminate\Support\ServiceProvider;
+
+class CacheLaraViewFragmentServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
